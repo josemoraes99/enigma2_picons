cb99008249a4f05ac8acd94ef50dee2f4fbceea5
Jose Moraes <josemoraes@mbpretina.lan>
2019-04-19 18:04:24 -0300

    [update_19_04_2019]

--- picons.py
+++ picons.py
@@ -13,8 +13,9 @@ import threading
 import time
 import uuid
 import urllib
+from functools import wraps
 
-__version__             = "0.2.9"
+__version__             = "0.2.10"
 __checkupdate__         = True
 __updateurl__           = "https://raw.githubusercontent.com/josemoraes99/enigma2_picons/master/picons.py"
 __e2dir__               = "/etc/enigma2/"
@@ -206,10 +207,57 @@ def update_progress(progress):
     sys.stdout.write(text)
     sys.stdout.flush()
 
+def retry(ExceptionToCheck, tries=4, delay=3, backoff=2, logger=None):
+    """Retry calling the decorated function using an exponential backoff.
+
+    http://www.saltycrane.com/blog/2009/11/trying-out-retry-decorator-python/
+    original from: http://wiki.python.org/moin/PythonDecoratorLibrary#Retry
+
+    :param ExceptionToCheck: the exception to check. may be a tuple of
+        exceptions to check
+    :type ExceptionToCheck: Exception or tuple
+    :param tries: number of times to try (not retry) before giving up
+    :type tries: int
+    :param delay: initial delay between retries in seconds
+    :type delay: int
+    :param backoff: backoff multiplier e.g. value of 2 will double the delay
+        each retry
+    :type backoff: int
+    :param logger: logger to use. If None, print
+    :type logger: logging.Logger instance
+    """
+    def deco_retry(f):
+
+        @wraps(f)
+        def f_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay
+            while mtries > 1:
+                try:
+                    return f(*args, **kwargs)
+                except ExceptionToCheck, e:
+                    msg = "%s, Retrying in %d seconds..." % (str(e), mdelay)
+                    if logger:
+                        logger.warning(msg)
+                    else:
+                        print msg
+                    time.sleep(mdelay)
+                    mtries -= 1
+                    mdelay *= backoff
+            return f(*args, **kwargs)
+
+        return f_retry  # true decorator
+
+    return deco_retry
+
+@retry(urllib2.URLError, tries=4, delay=3, backoff=2)
+def urlopen_with_retry(url):
+    return urllib2.urlopen(url)
+
 def downloadFile(url,file):
     global __progress__
     # print(file + " " + url)
-    imgdata = urllib2.urlopen(url).read()
+    # imgdata = urllib2.urlopen(url).read()
+    imgdata = urlopen_with_retry(url).read()
     # img = Image.open(cStringIO.StringIO(imgdata))
     with open(__localPiconDirectory__ + file, 'wb') as f:  
         f.write(imgdata)
@@ -253,7 +301,7 @@ def downloadPicons(f):
                 t = threading.Thread(target=downloadFile, args=(l[1], file[0]))
                 t.start()
                 threads.append(t)
-                while threading.active_count() > 5:
+                while threading.active_count() > 10:
                     time.sleep(0.1)
 
     while __progress__ < numDownloads:
